ec745466cce2ca90c306059b5346c5f425ca6240
SixtenNordegren <sixten.nordegren@gmail.com>
2024-09-16 12:32:04 +0200

    Last commit before large purge and moving to use stow.

--- main.sh
+++ main.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 FILEPATH="/home/sixten/Projects/dotfiles/"
 CONFIGS=$(ls "$FILEPATH/files")
-SCRIPT_DIR="$HOME/.config"
+# SCRIPT_DIR="$HOME/.config"
+SCRIPT_DIR="$HOME/Projects/dotfiles/experiment_directory"
 
+# (1) Select file.
+# (2) Go to target folder.
+# (3) If link exists in target folder, overwrite it.
+# (4) If file exists in repo but not in folder link to it.
+# (Not sure if to implement) If link exists in target folder but not in repo, delete it. But what
+# about external dependencies?
+
+# We are currently leaving old config files in the directory. Consider
+# making a to delete list.
 install_config() {
-	CHECKFILE="$FILEPATH/testdir/$FILE"
+	CHECKFILE="$FILEPATH/experiment_directory/$FILE"
 	if test -d "$CHECKFILE"; then
 		echo "Found file, do nothing."
 	else
 		REPOPATH="$FILEPATH/files/$FILE"
-		INSTALLPATH="$FILEPATH/testdir"
+		INSTALLPATH="$FILEPATH/experiment_directory"
 
-		ln "$REPOPATH" "$INSTALLPATH" && echo "Moved $FILE to $INSTALLPATH."
+		ln -f "$REPOPATH" "$INSTALLPATH" && echo "Moved $FILE to $INSTALLPATH."
 	fi
 }
 
