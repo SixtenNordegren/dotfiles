95c6d3213c9d02141a5f059826156da2724ce0d4
SixtenNordegren <sixten.nordegren@gmail.com>
2024-09-22 19:05:59 +0200

    Shortend main script.

--- main.sh
+++ main.sh
@@ -3,15 +3,10 @@ FILEPATH="$HOME/Projects/dotfiles"
 SCRIPT_DIR="$HOME/.config/"
 
 install_config() {
-	CHECKFILE="$FILEPATH/experiment_directory/$FILE"
-	if test -d "$CHECKFILE"; then
-		echo "Found file, do nothing."
-	else
-		REPOPATH="$FILEPATH/files/$FILE"
-		INSTALLPATH="$FILEPATH/experiment_directory"
+	REPOPATH="$FILEPATH/files/$FILE"
+	INSTALLPATH="$FILEPATH/experiment_directory"
 
-		ln -f "$REPOPATH" "$INSTALLPATH" && echo "Moved $FILE to $INSTALLPATH."
-	fi
+	ln -f "$REPOPATH" "$INSTALLPATH" && echo "Moved $FILE to $INSTALLPATH."
 }
 
 read -p "Is $SCRIPT_DIR the correct path for your config files(y/n)?" -n 1 -r
