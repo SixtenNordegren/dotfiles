d0285ae7e886967d29db5ee902e7285c21d9bfa6
SixtenNordegren <sixten.nordegren@gmail.com>
2024-09-22 16:56:08 +0200

    Now works on dotfiles. The script still has to be run from dotfile repos.

--- main.sh
+++ main.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
-FILEPATH="/home/sixten/Projects/dotfiles/"
-CONFIGS=$(ls "$FILEPATH/files")
-# SCRIPT_DIR="$HOME/.config"
-SCRIPT_DIR="$HOME/Projects/dotfiles/experiment_directory"
+FILEPATH="$HOME/Projects/dotfiles"
+SCRIPT_DIR="$HOME/.config/"
 
-# (1) Select file.
-# (2) Go to target folder.
-# (3) If link exists in target folder, overwrite it.
-# (4) If file exists in repo but not in folder link to it.
-# (Not sure if to implement) If link exists in target folder but not in repo, delete it. But what
-# about external dependencies?
-
-# We are currently leaving old config files in the directory. Consider
-# making a to delete list.
 install_config() {
 	CHECKFILE="$FILEPATH/experiment_directory/$FILE"
 	if test -d "$CHECKFILE"; then
@@ -29,9 +18,7 @@ read -p "Is $SCRIPT_DIR the correct path for your config files(y/n)?" -n 1 -r
 echo
 
 if [ "$REPLY" = "y" ]; then
-	for FILE in $CONFIGS; do
-		install_config
-	done
+	stow -v -R -t "$SCRIPT_DIR" files
 else
-	echo "Exiting, please edit the sctipt to input the correct filepath."
+	echo "Exiting, please edit the script and edit desired filepath."
 fi
