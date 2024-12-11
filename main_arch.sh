d370836c3129a4bcaf649aeef614e7832b93074d
SixtenNordegren <sixten.nordegren@gmail.com>
2024-12-11 12:03:16 +0100

    Fixed bug, scripts need to be run without sudo.

--- main_arch.sh
+++ main_arch.sh
@@ -1,32 +1,28 @@
 ## This is a script that automates the installation of packages for Arch Linux.
 
-# Ensure the script is run with sudo
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root"
-  exit
+# Ensure the script is not run as root
+if [ $EUID -eq 0 ]; then
+	echo "This script should not be run as root. Please run it as a regular user."
+	exit 1
 fi
 
 # Update the system and install essential packages
-pacman -Syu --noconfirm
-pacman -S --noconfirm xorg-xinit i3-wm firefox rofi python-pip cargo vifm neomutt picom base-devel git neovim zathura zathura-pdf-mupdf zathura-djvu alacritty nodejs npm ripgrep
-
-# Optional fun package
-cargo install typeracer 
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm xorg-xinit i3-wm firefox rofi python-pip cargo vifm neomutt picom base-devel git neovim zathura zathura-pdf-mupdf zathura-djvu alacritty nodejs npm ripgrep
 
 # Create necessary directories if they don't exist
-mkdir -p ~/Documents ~/Downloads ~/Projects ~/Tools
+mkdir -p ~/downloads ~/projects ~/tools
 
 # Stow dotfiles
 ./stow_dotfiles.sh
-if [ $? -ne 0 ]; then
-    echo "The stow command failed. Please check the error message and try again."
-    exit 1
+if [ $? -ne 0 ]; then # This does not work for some reason, need to add proper flags to stow_dotfiles.sh
+	echo "The stow command failed. Please check the error message and try again."
+	exit 1
 fi
 
 # Set up SSH key for GitHub
 rm -rf ~/.ssh
-mkdir -p ~/.ssh
-cd ~/.ssh
+mkdir -p ~/.ssh && cd ~/.ssh
 ssh-keygen -t rsa -b 4096 -C "sixten.nordegren@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
@@ -34,7 +30,7 @@ cat ~/.ssh/id_rsa.pub | xclip -sel clip
 read -p "SSH key copied to clipboard. Add it to your GitHub account and press enter to continue."
 
 # Install Hack Nerd Font
-cd ~/Downloads
+cd ~/downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Hack.zip
 unzip Hack.zip -d ~/.local/share/fonts
 fc-cache -fv
@@ -45,14 +41,15 @@ update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /
 
 # Source bashrc from dotfiles
 rm -f ~/.bashrc
-ln -s ~/Projects/dotfiles/files/bashrc ~/.bashrc
+ln -s ~/projects/dotfiles/files/bashrc ~/.bashrc
 
 # Set up xinit to start i3
 rm -f ~/.xinitrc
-echo "exec i3" > ~/.xinitrc
+echo "exec i3" >~/.xinitrc
 
 # Cleanup
-pacman -Rns $(pacman -Qdtq) --noconfirm
+pacman -Rns "$(pacman -Qdtq)" --noconfirm
+rm ~/Downloads/*
 
 # Finish up
 echo "Setup complete! Run 'startx' to start the i3 window manager."
