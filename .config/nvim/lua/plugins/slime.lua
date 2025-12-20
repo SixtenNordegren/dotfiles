9e30a7d93f2d8a589f7598248a834f93ffe15484
Sixten Nordegren <sixten.nordegren@gmail.com>
2025-12-21 00:20:24 +0100

    Removed unused plugins.

--- .config/nvim/lua/plugins/slime.lua
+++
@@ -1,1 +0,0 @@
-return { "jpalardy/vim-slime" }
--- .config/nvim/lua/settings_after.lua
+++ .config/nvim/lua/settings_after.lua
@@ -26,21 +26,9 @@ vim.opt.wrap = false
 vim.opt.spell = true
 vim.opt.tabstop = 3
 vim.g.auto_open_quickfix = 0
--- vim.cmd("colorscheme nightfly") -- TTY friendly colorscheme
--- Basetheme is alredy TTY friendly, do we really need this?
 -- vim.cmd([[syntax enable]])
 
 vim.opt.conceallevel = 2
 
--- Settings specific for slime
-vim.g.slime_target = "tmux"
-vim.g.slime_default_config = {
-	socket_name = "default",
-	target_pane = "{right-of}",
-	default_send_input = "top",
-	default_send_input_stay = "top",
-}
-
-vim.cmd("Copilot disable")
 -- vim.g.zig_fmt_autosave = 0
 vim.api.nvim_set_keymap("i", "<BS>", "<BS>", { noremap = true })
