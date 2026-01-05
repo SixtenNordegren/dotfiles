41f6534251723dc88b9c5ee159977e1500ac4649
Sixten Nordegren <sixten.nordegren@gmail.com>
2026-01-05 18:50:12 +0100

    Toggle tv changes

--- .config/i3/scripts/toggle_tv.sh
+++ .config/i3/scripts/toggle_tv.sh
@@ -3,14 +3,17 @@
 set -euxo pipefail
 
 RESOLUTION="1600x1200"
+TV_SINK="alsa_output.pci-0000_10_00.1.hdmi-stereo"
+OTHER_SINK="alsa_output.pci-0000_10_00.6.analog-stereo"
 
 connect_tv() {
-	xrandr --output "HDMI-1-5" --left-of "HDMI-3" --mode "1600x1200" >/dev/null
-	pactl set-sink-mute alsa_output.pci-0000_10_00.1.hdmi-stereo false &
+	xrandr --output "HDMI-1-5" --left-of "HDMI-3" --mode "$RESOLUTION" >/dev/null
+	pactl set-sink-mute "$TV_SINK" false &
 }
 
 disconnect_tv() {
 	xrandr --output "HDMI-1-5" --off >/dev/null
+	pactl set-sink-mute "$OTHER_SINK" false &
 }
 
 if xrandr | grep "HDMI-1-5" | grep -q "$RESOLUTION"; then
