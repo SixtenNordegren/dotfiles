05b4caf17e1167915cb52cda97352918cac03e45
Sixten Nordegren <sixten.nordegren@gmail.com>
2026-01-25 15:59:06 +0100

    QTbrowser.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -7,6 +7,6 @@ c.fonts.default_family = 'sans-serif'            # default font family
 c.fonts.default_size = '12pt'                    # default font size
 c.fonts.web.family.standard = 'sans-serif'       # standard web font
 c.fonts.web.family.fixed = 'monospace'           # fixed‑width font
-# c.terminal = 'xterm'
+c.terminal = 'ghostty'
 
 config.bind('pw', 'spawn -m --userscript qute-pass --always-show-selection')
